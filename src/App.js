2388cacb327b2ebebbf643d75d232ea24d6a2c15
Ragib Ajmal <ragibajmal@Ragib-ka-Mac-mini.local>
2021-10-22 19:23:49 +0530

    frontend website basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get('http://127.0.0.1:8000/todo/?format=json').then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+
+  function createPost() {
+    axios
+      .post('http://127.0.0.1:8000/todo/?format=json', {
+        content: "Hello World!"        
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  function deletePost(del_content) {
+    axios
+      .delete('http://127.0.0.1:8000/todo/?format=json',{data: {
+        id: del_content     
+      }})
+      .then((response) => {
+        console.log(del_content);
+        if (Array.isArray(del_content)) alert("All To-Do item deleted!");
+        else alert("To-Do item deleted!");
+        setPost(response.data);
+      });
+  }
+  
+  if (!post) return null;
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <button onClick={createPost}>Add To-Do</button>
+      <button onClick={() => deletePost(post)}>delete</button>
+      {post.map((item,index) => 
+          <li key={index}> 
+            {item.content } <button onClick={() => deletePost(item.id)}>delete</button>
+           </li>
+      )}
+            
     </div>
   );
 }
